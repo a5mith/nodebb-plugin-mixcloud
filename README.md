66aa2755a14589d647b684931211d97e0d7df293
a5mith <aaronsmith91@sky.com>
2014-05-31 15:25:37 +0100

    0.0.1
    First Attempt

---
+++ README.md
@@ -0,0 +1,13 @@
+# NodeBB MixCloud Plugin
+
+This NodeBB plugin allows users to embed MixCloud widgets inline in their posts.
+
+## Installation
+
+    npm install nodebb-plugin-mixcloud
+
+## Usage
+
+    Paste the URL into a post.
+    ???
+    Profit!
---
+++ library.js
@@ -0,0 +1,14 @@
+(function(module) {
+    "use strict";
+
+    var MixCloud = {},
+        embed = '<iframe src="//www.mixcloud.com/widget/iframe/?feed=http%3A%2F%2Fwww.mixcloud.com/$1&amp;replace=0&amp;embed_type=widget_standard" width="100%" frameborder="0" height="360"></iframe>'
+
+
+    MixCloud.parse = function(postContent, callback) {
+        postContent = postContent.replace(/<a href="(?:http?:\/\/)?(?:www\.)?(?:mixcloud\.com)\/?(.+)">.+<\/a>/g, embed);
+        callback(null, postContent);
+    };
+
+    module.exports = MixCloud;
+}(module));
