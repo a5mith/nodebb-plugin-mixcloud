5683711340f0421f22512902b551cb3b7927d00e
a5mith <aaronsmith91@sky.com>
2014-11-21 19:18:34 +0000

    Support for https protocol.

--- library.js
+++ library.js
@@ -4,7 +4,7 @@
     var MixCloud = {},
         embed = '<iframe src="//www.mixcloud.com/widget/iframe/?feed=http%3A%2F%2Fwww.mixcloud.com/$1&amp;replace=0&amp;embed_type=widget_standard" width="100%" frameborder="0" height="360"></iframe>'
 
-    var MixcloudUrl = /<a href="(?:http?:\/\/)?(?:www\.)?(?:mixcloud\.com)\/?(.+)">.+<\/a>/g;
+    var MixcloudUrl = /<a href="(?:https?:\/\/)?(?:www\.)?(?:mixcloud\.com)\/?(.+)">.+<\/a>/g;
 
 
     MixCloud.parse = function(data, callback) {
